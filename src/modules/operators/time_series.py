98ce0c00f9563d9d77646f68c1002b73b6cf79fc
KhangVinh <vinhkhang378@gmail.com>
2025-03-15 11:04:31 +0700

    test alpha

--- src/modules/operators/time_series.py
+++ src/modules/operators/time_series.py
@@ -10,3 +10,12 @@ class TimeSeriesOperators:
         summed_df[stocks] = x[stocks].rolling(window=days, min_periods=1).sum()
 
         return summed_df
+    
+    @classmethod
+    def ts_mean(cls, x: pd.DataFrame, days: int):
+        stocks = x.columns[1:]
+
+        mean_df = x.copy()
+        mean_df[stocks] = x[stocks].rolling(window=days, min_periods=1).mean()
+
+        return mean_df
--- test_alpha.py
+++ test_alpha.py
@@ -2,41 +2,45 @@ import numpy as np
 import pandas as pd
 
 from src.modules.operators import (
-    CrossSectionalOperators as cs, 
-    TimeSeriesOperators as ts,
-    TransformationalOperators as tf,
-    GroupOperators as gp,
-    ArithmeticOperators as ar,
-    LogicalOperators as lg
+    CrossSectionalOperators as CS, 
+    TimeSeriesOperators as TS,
+    TransformationalOperators as TF,
+    GroupOperators as GP,
+    ArithmeticOperators as AR,
+    LogicalOperators as LG
 )
 
 def test_alpha():
-    raw_data = pd.read_csv('close_price.csv')
+    close = pd.read_csv('close_price.csv')
 
-    print("===== TimeSeriesOperators =====")
-    ts_sum_matrix = ts.ts_sum(x=raw_data, days=10)
+    # ts_sum_matrix = ts.ts_sum(x=raw_data, days=10)
     # print(ts_sum_matrix)
 
-
-    print("===== CrossSectionalOperators =====")
-    ranked_data = cs.rank(x=raw_data)
-    print(ranked_data)
+    # ranked_data = cs.rank(x=raw_data)
+    # print(ranked_data)
 
     # test division
-    print("===== ArithmeticOperators =====")
     # divided_data = ar.divide(x=ranked_data, y=ranked_data)
     # print(divided_data)
 
     # test logical operators
-    print("===== LogicalOperators =====")
-    compared_data = lg.equal(x=raw_data, y=raw_data)
-    print(compared_data)
+    # compared_data = lg.equal(x=raw_data, y=raw_data)
+    # print(compared_data)
 
     # test combine alpha rank(ts_sum(close, 10))
-    result_matrix = cs.rank(x=ts.ts_sum(x=raw_data, days=10))
-    print(result_matrix)
+    # result_matrix = cs.rank(x=ts.ts_sum(x=raw_data, days=10))
+    # print(result_matrix)
 
+    trigger_trade_exp_matrix = LG.greater_than(x=close, y=TS.ts_mean(x=close, days=10))
+    print(trigger_trade_exp_matrix)
 
+    alpha_exp_matrix = CS.rank(x=close)
+    print(alpha_exp_matrix)
 
+    m_one_matrix = close.copy()
+    m_one_matrix.iloc[:, 1:] = -1
+
+    trigger_exit_exp_matrix = LG.less_than(x=close, y=TS.ts_mean(x=close, days=10))
+    print(trigger_exit_exp_matrix)
 
 test_alpha()
